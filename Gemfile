cf53da6ff6f7ed2c710c29fbbf5e38790d62659f
Boris Parak <araxxxx@gmail.com>
2012-02-04 21:04:02 +0100

    Included logger Log4r, mainly for debugging purposes

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "http://rubygems.org"
 ##
 gem "nagios-probe"
 gem "occi"
+gem "log4r"
--- check_opennebula.rb
+++ check_opennebula.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby
 
-####################################################
+###########################################################################
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
 ##
+##    http://www.apache.org/licenses/LICENSE-2.0
 ##
-##
-##
-##
-##
-####################################################
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+###########################################################################
 
 #
 $: << File.expand_path("..", __FILE__)
@@ -15,11 +20,19 @@ $: << File.expand_path("..", __FILE__)
 #
 require 'rubygems'
 require 'bundler/setup'
+
+#
+require 'nagios-probe'
+require 'log4r'
+
+#
 require 'lib/OpenNebulaOnedProbe'
 require 'lib/OpenNebulaOcciProbe'
 require 'lib/OpenNebulaEconeProbe'
 require 'lib/OptparseNagiosProbe'
-require 'nagios-probe'
+
+#
+include Log4r
 
 #
 begin
@@ -36,6 +49,11 @@ begin
     else
       raise Exception.new("This probe cannot check the specified service")
   end
+
+  logger = Logger.new 'OpenNebulaProbe'
+  logger.outputters = Outputter.stdout
+  logger.level = ERROR unless options.debug
+  probe.logger = logger
 
   probe.run
 
